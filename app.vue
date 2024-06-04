34ae89c8cab5aa61905fccc41a2f613b89eec01e
nikitaWebApps <nikitagolovlev.design@gmail.com>
2024-06-04 13:01:55 +0500

    added Types

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup>
 const { data } = await useAsyncData('data', () => queryContent().findOne());
 const sortedByDate = data.value.body.sort((a, b) => (a.date > b.date) ? 1 : (a.date < b.date) ? -1 : 0)
+
 useSeoMeta({
   title: 'Общее расписание на ' + data.value.title.toLowerCase(),
   description: 'Общее расписание обучающих семинаров и вебинаров компании Фармацевтикал Косметик',
